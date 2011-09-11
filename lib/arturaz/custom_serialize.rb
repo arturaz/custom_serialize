1d2a854f05149392cc63cfb1cc3328f7c6fc3ebb
Arturas Slajus <x11@arturaz.net>
2011-09-11 17:54:22 +0300

    Erm, stuff n changes.

--- lib/arturaz/custom_serialize.rb
+++ lib/arturaz/custom_serialize.rb
@@ -24,36 +24,22 @@ module Arturaz
       # Default values for :serialize and :unserialize converts to and from
       # JSON.
       #
-      # This method utilizes #after_find callback so if you want to use it too
-      # remember to call super:
-      #
-      # <pre>
-      # def after_find
-      #   super
-      #
-      #   # do your own stuff here
-      # end
-      # </pre>
-      #
       def custom_serialize(*args)
-        options = args.pop
+        options = args.last.is_a?(Hash) ? args.pop : {}
         options.reverse_merge!(
           :serialize => Proc.new { |value| value.to_json },
           :unserialize => Proc.new { |value| JSON.parse(value) }
         )
         attributes = args
 
-        define_method(:after_find) do
+        after_find :custom_unserialize_attributes
+        define_method(:custom_unserialize_attributes) do
           attributes.each do |attribute|
             send(:"#{attribute}=", options[:unserialize].call(send(attribute)))
+            changed_attributes.delete attribute.to_s
           end
 
-          begin
-            super
-            # There may not be any super method
-          rescue NoMethodError
-            true
-          end
+          super() if defined?(super)
         end
 
         define_method(:custom_serialize_attributes) do
@@ -66,7 +52,7 @@ module Arturaz
 
         before_save :custom_serialize_attributes
         # Restore attributes changed by #custom_serialize_attributes
-        after_save :after_find
+        after_save :custom_unserialize_attributes
       end
     end
   end
